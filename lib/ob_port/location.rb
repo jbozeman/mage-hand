19b336327ad2f1012548346ae049ca16cd3a3b86
Steven Hammond <shammond@northpub.com>
2012-07-05 10:18:21 -0400

    Basic documentation for the location class

--- lib/ob_port/location.rb
+++ lib/ob_port/location.rb
@@ -1,5 +1,12 @@
 module MageHand
+  # Stores the location of a campaign as a lat, lng pair.
+  # @author Steven Hammond (@shammond42)
   class Location < Base
-    attr_accessor :lat, :lng
+
+    # @returns [String] the decimal latitude
+    attr_accessor :lat
+
+    # @returns [String] the decimal latitude
+    attr_accessor :lng
   end
 end
