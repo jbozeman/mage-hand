3f016a4aca085b489ce2733cc477cee8c0b7c7b8
Steven Hammond <shammond@northpub.com>
2012-07-08 09:59:07 -0400

    Documented the error classes.

--- lib/ob_port/errors.rb
+++ lib/ob_port/errors.rb
@@ -1,6 +1,14 @@
 module MageHand
+  # raised when calls to mage hand are made without proper initialization.
   class MageHandNotInitialized < StandardError; end
+
+  # raised as a result of an oauth configuration error.
   class OAuthConfigurationError < StandardError; end
+
+  # raised when an attempt is made to create a new wiki page with the same name
+  # as one that already exists.
   class WikiPageExists < StandardError; end
+
+  # raised when Obsidian Portal reports some error saving a wiki page
   class WikiPageError < StandardError; end
 end
